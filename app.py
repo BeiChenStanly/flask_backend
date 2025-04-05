20b5d75fd6f924cfbbb97b9daab9acbd9fa6731f
beichenstanly <zhengxgen@163.com>
2025-04-05 18:17:51 +0800

    cors

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from api.predict import predict_route
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app,resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(predict_route, url_prefix='/api')
 
 if __name__ == '__main__':
